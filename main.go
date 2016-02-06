8678af45085ae2d4c127cd810f3fffb0baf80972
Zack Scholl <zack.scholl@gmail.com>
2016-02-06 00:34:48 -0500

    Trying to get IP

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/celrenheit/spider"
 	"github.com/celrenheit/spider/schedule"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -28,38 +30,41 @@ func main() {
 	// fmt.Println(p)
 	// Close()
 
-	// router := gin.Default()
-	// router.LoadHTMLGlob("templates/*")
-	// router.GET("/", func(c *gin.Context) {
-	// 	day := Today()
-	// 	c.Redirect(http.StatusMovedPermanently, "/"+day)
-	// })
-	// router.GET("/:year/:month/:day", func(c *gin.Context) {
-	// 	year := c.Param("year")
-	// 	month := c.Param("month")
-	// 	daynum := c.Param("day")
-	// 	day := year + "/" + month + "/" + daynum
-	// 	// Open()
-	// 	var scienceNews ScienceNewsGroup
-	// 	scienceNews.Get(day)
-	// 	var inTheaters InTheatersGroup
-	// 	inTheaters.Get(day)
-	// 	var groceryList GroceryListGroup
-	// 	groceryList.Get(day)
-	// 	var weather WeatherData
-	// 	weather.Get(day)
-	// 	// Close()
-	//
-	// 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-	// 		"Date":        day,
-	// 		"ScienceNews": scienceNews.Data,
-	// 		"InTheaters":  inTheaters.Data,
-	// 		"Groceries":   groceryList.Data,
-	// 		"Weather":     weather,
-	// 	})
-	// })
-	//
-	// router.Run(":8001")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		day := Today()
+		fmt.Println("-------")
+		fmt.Println(c.ClientIP())
+		fmt.Println("-------")
+		c.Redirect(http.StatusMovedPermanently, c.ClientIP()+"/"+day)
+	})
+	router.GET("/:year/:month/:day", func(c *gin.Context) {
+		year := c.Param("year")
+		month := c.Param("month")
+		daynum := c.Param("day")
+		day := year + "/" + month + "/" + daynum
+		// Open()
+		var scienceNews ScienceNewsGroup
+		scienceNews.Get(day)
+		var inTheaters InTheatersGroup
+		inTheaters.Get(day)
+		var groceryList GroceryListGroup
+		groceryList.Get(day)
+		var weather WeatherData
+		weather.Get(day)
+		// Close()
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Date":        day,
+			"ScienceNews": scienceNews.Data,
+			"InTheaters":  inTheaters.Data,
+			"Groceries":   groceryList.Data,
+			"Weather":     weather,
+		})
+	})
+
+	router.Run(":8001")
 
 	// Exit 5 seconds later to let time for the request to be done.
 	<-time.After(3 * time.Second)
